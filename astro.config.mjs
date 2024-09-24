c2dd2cb6ac1cfd3e6a9a576a6bfc18c29afea442
Junior Calva <juniorcalva40@gmail.com>
2024-09-24 10:55:05 -0500

    📱 Add component Section Container for max-with
    Add styles for improve resposive desing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
-});
+	integrations: [tailwind(), icon()],
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['es', 'en', 'pt-br']
+	}
+})
